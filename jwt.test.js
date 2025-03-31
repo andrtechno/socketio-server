0daa1e22c0e81e620c01bafcb1e647de0a02950c
technoid <andrew.panix@gmail.com>
2025-03-31 17:13:07 +0300

    remove

--- jwt.test.js
+++
@@ -1,4 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const token = jwt.sign({ username: "test_user" }, "your_secret_key", { expiresIn: "1h" });
-console.log("Ваш токен:", token);
--- utils.js
+++
@@ -1,4 +0,0 @@
-export function isValidToken(token) {
-    // Логика проверки токена
-    return token === 'valid_token';
-}
