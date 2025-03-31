9e1ea7758878f1d5f9b1d146035d75d995c1da7d
technoid <andrew.panix@gmail.com>
2025-03-31 11:23:33 +0300

    1

--- redis_con.js
+++ redis_con.js
@@ -1,20 +1,25 @@
 import redis from 'redis';
+import logger from "./logger.js";
 
 let redisClient;
 
-async function connectRedis() {
+export function redisConf() {
     redisClient = redis.createClient({
         database: process.env.REDIS_DB,
         socket: {
             host: process.env.REDIS_HOST, // Адрес сервера Redis
-            port: process.env.REDIS_PORT,        // Порт сервера Redis
+            port: process.env.REDIS_PORT, // Порт сервера Redis
         }
     });
 
-    redisClient.on('error', (err) => console.error('Redis Client Error', err));
+    redisClient.on('error', (err) => logger.error('Redis Client Error'));
+    return redisClient;
+}
 
-    await redisClient.connect();
-    console.log('Redis подключен');
+async function connectRedis() {
+
+    await redisConf().connect();
+    logger.info('Redis подключен');
     return redisClient;
 }
 
--- server2.js
+++ server2.js
@@ -3,13 +3,12 @@ import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
 import {Server} from 'socket.io';
-import { initRedis } from './redis.js';
-import { socketHandler } from './socket.js';
 import fs from 'fs';
-import util from 'util';
-import winston from 'winston';
-
 import logger from './logger.js';
+import {billingNamespace} from "./chats/billing.js";
+import {createAdapter} from '@socket.io/redis-adapter';
+import {redisConf} from "./redis_con.js";
+import {authMiddleware} from "./auth.js";
 
 
 
@@ -42,64 +41,80 @@ const io = new Server(server, {
 // HTTP API для отправки сообщений в WebSocket
 app.use(express.json());
 
+let pubClient, subClient;
 
-// Инициализация Redis
-const redisClient = await initRedis(io);
-socketHandler(io, redisClient);
+async function setupRedisAdapter() {
+    pubClient = redisConf(); // Получаем клиент Redis
+    subClient = pubClient.duplicate();
 
+    await Promise.all([pubClient.connect(), subClient.connect()]);
 
-
-
-
-app.post("/send", (req, res) => {
-    const {channel, message} = req.body;
-
-    // Если канал и сообщение указаны, отправляем сообщение в канал
-    if (channel && message) {
-
+    if (pubClient && subClient) {
+        io.adapter(createAdapter(pubClient, subClient));
+        logger.info('✅ Redis Adapter подключен');
+    }
+}
 
 
+Promise.resolve().then(setupRedisAdapter).then(() => {
+    //Middleware
+    io.use(authMiddleware);
 
-        // io.of("/billing").to(channel).emit("event", message);
-        io.of("/billing").to(channel).timeout(5000).emit("event", message, (err, responses) => {
-            if (err) {
-                logger.info('the client did not acknowledge the event in the given delay');
+//transactionNamespace(io);
+    billingNamespace(io).then(r => {
+        app.post("/send", (req, res) => {
+            const {channel, message} = req.body;
+
+            // Если канал и сообщение указаны, отправляем сообщение в канал
+            if (channel && message) {
+
+
+                // io.of("/billing").to(channel).emit("event", message);
+                io.of("/billing").to(channel).timeout(5000).emit("event", message, (err, responses) => {
+                    if (err) {
+                        logger.info('the client did not acknowledge the event in the given delay');
+                    } else {
+                        if (responses[0] && responses[0].status === "accepted") {
+                            logger.info("Подтвердил получение сообщения:", responses);
+                        } else {
+                            logger.info("Не отправил подтверждение! записываем в Redis");
+                            redisClient.rPush(`channel:${channel}:messages`, JSON.stringify(message));
+                        }
+                    }
+                });
+
+
+                logger.info(`Sent to channel ${channel}:`, message);
+                //res.status(200).send("Message sent");
+                res.status(200).json({success: true});
             } else {
-                if (responses[0] && responses[0].status === "accepted") {
-                    logger.info("Подтвердил получение сообщения:", responses);
-                } else {
-                    logger.info("Не отправил подтверждение! записываем в Redis");
-                    redisClient.rPush(`channel:${channel}:messages`, JSON.stringify(message));
-                }
+                //res.status(400).send("Channel and message are required");
+                res.status(400).json({success: false});
             }
         });
 
+    });
 
+    server.on('error', (e) => {
+        if (e.code === 'EADDRINUSE') {
+            logger.error('Address in use, retrying...');
+            setTimeout(() => {
+                server.close();
+                server.listen(PORT, () => {
+                    logger.info(`Socket.io сервер пере запущен на порту ${PORT}`);
+                });
+            }, 1000);
+        }
+    });
 
+    server.listen(PORT, () => {
+        logger.info(`Socket.io сервер запущен на порту ${PORT}`,'ffff');
+    });
 
-        logger.info(`Sent to channel ${channel}:`, message);
-        //res.status(200).send("Message sent");
-        res.status(200).json({success: true});
-    } else {
-        //res.status(400).send("Channel and message are required");
-        res.status(400).json({success: false});
-    }
 });
 
 
 
-server.on('error', (e) => {
-    if (e.code === 'EADDRINUSE') {
-        logger.error('Address in use, retrying...');
-        setTimeout(() => {
-            server.close();
-            server.listen(PORT, () => {
-                logger.info(`Socket.io сервер пере запущен на порту ${PORT}`);
-            });
-        }, 1000);
-    }
-});
 
-server.listen(PORT, () => {
-    logger.info(`Socket.io сервер запущен на порту ${PORT}`,'ffff');
-});
+
+
